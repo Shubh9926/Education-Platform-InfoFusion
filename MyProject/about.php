5a5383a0feedaaa2e518a51abf512838d8b4c695
Shubhendra Singh <shubhendra.ss2002@gmail.com>
2024-03-11 21:47:48 +0530

    This platform provides various functionalities with one special assistance functionality i.e. help/support/ask query through which users can connect with an ease.
    Education is like a basic need similar to food, clothing and house nowadays. Looking at a present world education field is progressing day-by-day one gain education to not to only gain knowledge about specific thing but also to become a skillful in that field. In the modern tech world,  skilled people are demanded who compete with the existing problem and futuristic real time problems as well. The goal of this work is to provide a platform for learners to seek knowledge and form a strong foundation in their journey of mastering their respective field because if your foundation is strong then the building will be built firmly.

---
+++ MyProject/about.php
@@ -0,0 +1,221 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Why choose us?</h3>
+         <p>Our vision to reach the students or learners and provide them efficient learning with the help of easy to access notes and lectures and many more.</p>
+         <!--a href="courses.html" class="inline-btn">our courses</a-->
+      </div>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-graduation-cap"></i>
+         <div>
+            <h3>Multiple</h3>
+            <span>online courses</span>
+         </div>
+      </div>
+
+      <!--div class="box">
+         <i class="fas fa-user-graduate"></i>
+         <div>
+            <h3>+25k</h3>
+            <span>brilliants students</span>
+         </div>
+      </div-->
+
+      <div class="box">
+         <i class="fas fa-chalkboard-user"></i>
+         <div>
+            <!--h3>+5k</h3-->
+            <h3>Expert teachers</h3>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-briefcase"></i>
+         <div>
+            <h3>Good learning</h3>
+            <!--span>job placement</span-->
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading">student's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <p>Very good motive of this platform that "Our vision to reach the students or learners and provide them efficient learning with the help of easy to access notes and lectures and many more".</p>
+         <div class="user">
+            <img src="images/pic-2.jpg" alt="">
+            <div>
+               <h3>pragati</h3>
+               <!--div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div-->
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>Very good experience,such a great platform</p>
+         <div class="user">
+            <img src="images/pic-3.jpg" alt="">
+            <div>
+               <h3>sakshi</h3>
+               <!--div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div-->
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>Good services and wonderful experience</p>
+         <div class="user">
+            <img src="images/pic-4.jpg" alt="">
+            <div>
+               <h3>Nitish</h3>
+               <!--div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div-->
+            </div>
+         </div>
+      </div>
+
+      <!--div class="box">
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo fugiat, quaerat voluptate odio consectetur assumenda fugit maxime unde at ex?</p>
+         <div class="user">
+            <img src="images/pic-5.jpg" alt="">
+            <div>
+               <h3>john deo</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div-->
+
+      <!--div class="box">
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo fugiat, quaerat voluptate odio consectetur assumenda fugit maxime unde at ex?</p>
+         <div class="user">
+            <img src="images/pic-6.jpg" alt="">
+            <div>
+               <h3>john deo</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div-->
+
+      <!--div class="box">
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo fugiat, quaerat voluptate odio consectetur assumenda fugit maxime unde at ex?</p>
+         <div class="user">
+            <img src="images/pic-7.jpg" alt="">
+            <div>
+               <h3>john deo</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div-->
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+<!--?php include 'components/footer.php'; ?-->
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
